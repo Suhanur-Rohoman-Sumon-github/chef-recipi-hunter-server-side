896b663a4289072f15ff3f000ca69e33360047ea
su44248124 <sumonsuhanurrohoman@gmail.com>
2023-05-01 13:09:46 -0600

    chef dta added

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const express = require(express)
+const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
+
+
+app.get('/',(req , res)=>{
+    res.send('dragin is running')
+})
+
+app.listen(port, ()=>{
+    console.log(`dragon is runing in prort ${port} `)
+})
