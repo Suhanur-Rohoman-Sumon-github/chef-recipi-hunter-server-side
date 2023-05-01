fe634ea38de5cc313e65ee359d9414ffa22254f3
su44248124 <sumonsuhanurrohoman@gmail.com>
2023-05-01 16:05:45 -0600

    single chef added

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
+var cors = require('cors')
 
+const chef = require('./data/chef.json')
+const servises = require('./data/sarvises.json')
+
+app.use(cors())
 
 app.get('/',(req , res)=>{
     res.send('dragin is running')
+})
+
+app.get('/chef',(req,res)=>{
+    res.send(chef)
+})
+app.get('/servises',(req,res)=>{
+    res.send(servises)
 })
 
 app.listen(port, ()=>{
